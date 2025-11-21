ff98cd2d50874c29a5ec0b700b7488f7060ca43d
Adrian <adri016iron@gmail.com>
2025-11-21 09:26:47 +0100

    Cambios en el mensaje de alert de los inmuebles

--- src/app/components/card-casa/card-casa.ts
+++ src/app/components/card-casa/card-casa.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-card-casa',
@@ -12,4 +13,24 @@ export class CardCasa {
   @Input() ubicacion!: string;
   @Input() precio!: string;
   @Input() descripcion!: string;
+
+  maximizar() {
+    Swal.fire({
+    text: `${this.descripcion}`,
+    imageUrl: `${this.foto}`,
+    imageWidth: 400,
+    imageAlt: 'Imagen del inmueble',
+    html: `
+      <h2 style="font-size: 22px; font-weight: bold;">🏡 ${this.titulo}</h2>
+      <br>
+      <p>📍 <strong>${this.ubicacion}</strong></p>
+      <p style="color:#1e4dd8; font-size: 22px; font-weight: bold;">${this.precio}</p>
+      <p style="text-align:left; font-size: 14px; color: #444; margin-top:10px;">
+        ${this.descripcion}        
+      </p>
+    `,
+    confirmButtonText: 'Cerrar',
+    confirmButtonColor: '#2563eb'
+  });
+  }
 }
