477feabe59b2c15366adbb13df21ed799cd35964
JoseCalderon18 <josse180804@gmail.com>
2025-11-25 13:22:27 +0100

    Mejoros en el home y cleinte

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CardHome } from '../../components/card-home/card-home';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [CardHome],
+  imports: [CardHome, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
