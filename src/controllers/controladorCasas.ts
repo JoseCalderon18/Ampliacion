96a6658b546bb3a229bd97cf0b4bc4ed87f91e7e
gonzozzz <25015106+gonzozzz@users.noreply.github.com>
2025-11-26 14:02:57 +0100

    mejoras en inmuebles

--- src/controllers/controladorCasas.ts
+++ src/controllers/controladorCasas.ts
@@ -8,8 +8,8 @@ export class CasasController {
 
   private casas: Casa[] = [
     {
-      titulo: "Chalet independiente ...",
-      direccion: "Camino Viejo...",
+      titulo: "Chalet independiente",
+      direccion: "Camino Viejo",
       ciudad: "Talavera",
       provincia: "Toledo",
       cp: "45600",
@@ -30,11 +30,40 @@ export class CasasController {
       anoConstruccion: 1980,
       estado: "Buena conservación",
       extras: [ "Piscina", "Jardín" ],
-      imagenes: [ "assets/img/chalet.jpg" ]
+      imagenes: [
+  "assets/img/Chalet independiente/foto1.jpg",
+  "assets/img/Chalet independiente/foto2.jpg",
+  "assets/img/Chalet independiente/foto3.jpg",
+  "assets/img/Chalet independiente/foto4.jpg",
+  "assets/img/Chalet independiente/foto5.jpg",
+  "assets/img/Chalet independiente/foto6.jpg",
+  "assets/img/Chalet independiente/foto7.jpg",
+  "assets/img/Chalet independiente/foto8.jpg",
+  "assets/img/Chalet independiente/foto9.jpg",
+  "assets/img/Chalet independiente/foto10.jpg",
+  "assets/img/Chalet independiente/foto11.jpg",
+  "assets/img/Chalet independiente/foto12.jpg",
+  "assets/img/Chalet independiente/foto13.jpg",
+  "assets/img/Chalet independiente/foto14.jpg",
+  "assets/img/Chalet independiente/foto15.jpg",
+  "assets/img/Chalet independiente/foto16.jpg",
+  "assets/img/Chalet independiente/foto17.jpg",
+  "assets/img/Chalet independiente/foto18.jpg",
+  "assets/img/Chalet independiente/foto19.jpg",
+  "assets/img/Chalet independiente/foto20.jpg",
+  "assets/img/Chalet independiente/foto21.jpg",
+  "assets/img/Chalet independiente/foto22.jpg",
+  "assets/img/Chalet independiente/foto23.jpg",
+  "assets/img/Chalet independiente/foto24.jpg",
+  "assets/img/Chalet independiente/foto25.jpg",
+  "assets/img/Chalet independiente/foto26.jpg",
+  "assets/img/Chalet independiente/foto27.jpg",
+  "assets/img/Chalet independiente/foto28.jpg"
+]
     },{
     titulo: "Finca de regadío con buen acceso",
-    direccion: "EX384",
-    ciudad: "Madrigal de la Vera",
+    direccion: "Madrigal de la Vera",
+    ciudad: "Extremadura",
     provincia: "Cáceres",
     cp: "10400",
     pais: "España",
@@ -54,10 +83,12 @@ export class CasasController {
     anoConstruccion: 0, // No aplica
     estado: "Parcela disponible",
     extras: ["Agua corriente disponible", "Electricidad disponible", "Jardines"],
-    imagenes: ["assets/img/casa-BORRAR.png"]
+    imagenes: ["assets/img/casa-BORRAR.png",
+    "assets/img/casa-BORRAR.png",
+    "assets/img/casa-BORRAR.png"]
   },{
-  titulo: "Casa adosada para reformar en Candeleda",
-  direccion: "", // No se especifica en el anuncio
+  titulo: "Casa adosada para reformar",
+  direccion: "Privada", // No se especifica en el anuncio
   ciudad: "Candeleda",
   provincia: "Ávila",
   cp: "5480",
@@ -88,10 +119,12 @@ export class CasasController {
     "Autobús urbano"
   ],
   imagenes: [
+    "assets/img/casa-BORRAR.png",
+    "assets/img/casa-BORRAR.png",
     "assets/img/casa-BORRAR.png"
   ]
 },{
-  titulo: "Finca con vivienda y piscina en Villanueva de la Vera",
+  titulo: "Finca con vivienda y piscina",
   direccion: "EX 203",
   ciudad: "Villanueva de la Vera",
   provincia: "Cáceres",
@@ -126,9 +159,81 @@ export class CasasController {
     "Autobús urbano"
   ],
   imagenes: [
+    "assets/img/casa-BORRAR.png",
+    "assets/img/casa-BORRAR.png",
+    "assets/img/casa-BORRAR.png"
+  ]
+},{
+  titulo: "Local en venta",
+  direccion: "Corredera del Cristo, 28",
+  ciudad: "Talavera de la Reina",
+  provincia: "Toledo",
+  cp: "45600",
+  pais: "España",
+  lat: 39.96015,
+  lng: -4.83297,
+  precio: 120000,
+  precioM2: 1714, // 120000 / 70
+  antiguedad: 0, // No se indica año de construcción
+  tipo: "Local comercial",
+  plantas: 1,
+  metrosConstruidos: 70,
+  terreno: 0, // No aplica en locales
+  habitaciones: 0, // No aplica
+  banos: 1, // 1 aseo
+  descripcion: "Local en casco antiguo de 70 m² con agua y luz. Es cuadrado, muy bien situado frente al mercado de Abastos. Tiene 3 estancias hechas con pladur que se pueden quitar y dejar el espacio diáfano.",
+  orientacion: "N/A",
+  anoConstruccion: 0, // No se especifica
+  estado: "Buena conservación",
+  extras: [
+    "1 escaparate",
+    "3 estancias desmontables",
+    "Instalación de agua",
+    "Instalación eléctrica",
+    "Cierres"
+  ],
+  imagenes: [
+    "assets/img/casa-BORRAR.png",
+    "assets/img/casa-BORRAR.png",
+    "assets/img/casa-BORRAR.png" // Puedes cambiarlo por la ruta correcta
+  ]
+},{
+  titulo: "Local en venta en Talavera de la Reina",
+  direccion: "Corredera del Cristo, 28",
+  ciudad: "Talavera de la Reina",
+  provincia: "Toledo",
+  cp: "45600",
+  pais: "España",
+  lat: 39.96015,
+  lng: -4.83297,
+  precio: 120000,
+  precioM2: 1714, // 120000 / 70
+  antiguedad: 0, // No especificado
+  tipo: "Local comercial",
+  plantas: 1,
+  metrosConstruidos: 70,
+  terreno: 0, // No aplica
+  habitaciones: 0, // No aplica
+  banos: 1, // 1 aseo
+  descripcion: "Local en casco antiguo de 70 m² con agua y luz. Es cuadrado, muy bien situado frente al mercado de Abastos. Tiene 3 estancias hechas con pladur que se pueden quitar y dejar el espacio diáfano.",
+  orientacion: "N/A",
+  anoConstruccion: 0, // No especificado
+  estado: "Buena conservación",
+  extras: [
+    "1 escaparate",
+    "3 estancias desmontables",
+    "Instalación de agua",
+    "Instalación eléctrica",
+    "Cierres"
+  ],
+  imagenes: [
+    "assets/img/casa-BORRAR.png",
+    "assets/img/casa-BORRAR.png",
     "assets/img/casa-BORRAR.png"
   ]
 }
+
+
 
   ];
 
